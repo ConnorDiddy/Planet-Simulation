ca5072f3a29fdab36d0ed74ccf49728cb6af4d2b
ConnorDiddy <connordidericksen@gmail.com>
2022-04-07 23:33:51 -0600

    updates for drawing lines

--- main.py
+++ main.py
@@ -41,6 +41,18 @@ class Planet:
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
         pygame.draw.circle(win, self.color, (x,y), self.radius)
+        
+        if len(self.orbit) > 2:
+            updated_points = []
+
+            for point in self.orbit:
+                x, y = point
+                x = x * self.SCALE + WIDTH / 2
+                y = y * self.SCALE + HEIGHT / 2
+
+                updated_points.append((x, y))
+
+                pygame.draw.lines(win, self.color, False, updated_points, 2)
 
     def attraction(self, other):
         other_x, other_y = other.x, other.y
@@ -86,9 +98,13 @@ def main():
 
     sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_vel = 29.783 * 1000
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.3 * 10**23)
+    mars.y_vel = 24.077 * 1000
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GRAY, 3.30 * 10**23)
+    mercury.y_vel = 47.4 * 1000
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus.y_vel = -35.02 * 1000
 
     sun.sun = True
 
